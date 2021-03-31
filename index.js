328abc1af30908207fb70bc959ded9eee21d536d
Javier <JavierRLeon@protonmail.com>
2021-03-31 14:40:16 -0400

    commit tree f9f4900a05f565401a4a6e3e67091bcb7cd8e5b5

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat () {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat () {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat () {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat () {
+    cats.shift();
+}
+
+function appendCat (name) {
+    const appendNewCats = [...cats, name];
+    return appendNewCats
+}
+
+function prependCat (name) {
+    const prependNewCats = [name, ...cats];
+    return prependNewCats
+}
+
+function removeLastCat() {
+    return cats.slice(0, cats.length -1);
+}
+
+function removeFirstCat () {
+    return cats.slice(1);
+}
